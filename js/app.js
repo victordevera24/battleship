a91cd6e3a0b9baec43204e0ac393ea6215423a57
victordevera24 <victordevera24@gmail.com>
2022-04-08 21:48:28 -0700

    adding start button

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 //function here ----------------------------------------------------------------
 function buildGrid(str){
     for(var i=1;i<101;i++){
-        let newSquare = `<div id="${i}" class="square"></div>`
+        let newSquare = `<div id="${i}" class="square"></div>`;
         $(str).append(newSquare);
     }
 }
@@ -12,16 +12,33 @@ function hidePlayerGrid(){
 function showPlayerGrid(){
     $('#playerGrid').css('display', '');
 }
-function creatShip(num){
-    
+function startButtonText(){
+    $('#text').html("<button class='startGame'>Start Game</button>");
 }
+function placeShipsText(){
+    $('#text').text("Place ships");
+}
+// function creatShip(num){
+//     for (let i = 0; i < num; i++) {
+//         let ship = 
+//     }
+// }
 
-// building grid called here ------------------------------------
-buildGrid("#mainGrid")
-buildGrid("#playerGrid")
-hidePlayerGrid()
-showPlayerGrid()
-hidePlayerGrid()
+// start game    ------------------------------------
+startButtonText();
+buildGrid("#mainGrid");
+buildGrid("#playerGrid");
+hidePlayerGrid();
+
+
 
 //text here -----------------------------------------
-$('#text').text('Click ship, then hover over grid to place')
+
+
+
+
+//events -------------------------------------------------------
+$('.startGame').click(function(evt){
+    console.log('clicked start game');
+    placeShipsText();
+})
