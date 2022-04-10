3a0f0ca74c2a1fa0b0775c6d7607cc455e701474
victordevera24 <victordevera24@gmail.com>
2022-04-10 16:00:29 -0700

    adding choose ship div

--- js/app.js
+++ js/app.js
@@ -7,10 +7,10 @@ function buildGrid(str){
     }
 }
 function hidePlayerGrid(){
-    $('#playerGrid').css('display', 'none');
+    $('#playerGrid').toggle();
 }
-function showPlayerGrid(){
-    $('#playerGrid').css('display', '');
+function hideChooseShip(){
+    $('#chooseShip').toggle();
 }
 function startButtonText(){
     $('#text').html("<button class='startGame'>Start Game</button>");
@@ -18,17 +18,29 @@ function startButtonText(){
 function placeShipsText(){
     $('#text').text("Place ships");
 }
-// function creatShip(num){
-//     for (let i = 0; i < num; i++) {
-//         let ship = 
-//     }
-// }
+function createShip(num){
+    let newShip = `<div id="ship${num}" class="ship ${num} ship${num}"></div>`;
+    let makingShip = ".ship"+num
+    $('#chooseShip').append(newShip);
+    for (let i = 0; i < num; i++) {
+        $(makingShip).append("<div class='shipSquare'></div>");
+    }
+}
+function createSecond3LongShip(){
+    let newShip = `<div id="second3Ship" class="ship 3 ship3"></div>`;
+    $('#chooseShip').append(newShip);
+    for (let i = 0; i < 3; i++) {
+        $('#second3Ship').append("<div class='shipSquare'></div>");
+    }
+
+}
 
 // start game    ------------------------------------
 startButtonText();
 buildGrid("#mainGrid");
 buildGrid("#playerGrid");
 hidePlayerGrid();
+hideChooseShip();
 
 
 
@@ -40,5 +52,11 @@ hidePlayerGrid();
 //events -------------------------------------------------------
 $('.startGame').click(function(evt){
     console.log('clicked start game');
+    hideChooseShip();
     placeShipsText();
+    createShip(2);
+    createShip(3);
+    createSecond3LongShip();
+    createShip(4);
+    createShip(5);
 })
