b79c4fdd010341ecc8576c8dfbf591ac2de33ee8
victordevera24 <victordevera24@gmail.com>
2022-04-11 15:48:08 -0700

    adding computer random map

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ let currentShipClicked = null;
 let currentShipVertical = false;
 let holdingShip = false;
 let playerMap = {};
+let computerMap = {};
 let attackPhase = false;
 // start game    -------------------------------------------------------
 startButtonText();
@@ -57,7 +58,9 @@ function createSecond3LongShip(){
 function makePlayerMap(){
     for (let i=1; i<101;i++){
         playerMap[i]=0;
+        computerMap[i]=0;
     }
+    randomComputerShips();
 }
 function markPlayerGrid(){
     for (const box in playerMap){
@@ -80,6 +83,56 @@ function fillPlayerGrid(){
     for (const box in playerMap){
         if(playerMap[box]===1){
             $('#playerGrid #'+box).css('background-color','rgb(55,54,51)');
+        }
+    }
+}
+function randomComputerShips(){
+    let firstShipStart = Math.floor(Math.random()*100)+1;
+    while(firstShipStart.toString().endsWith('1')){
+        firstShipStart = Math.floor(Math.random()*100)+1;
+    }
+    computerMap[firstShipStart] = 1;
+    computerMap[firstShipStart-1] = 1;
+    let secondShipStart = Math.floor(Math.random()*100)+1;
+    for(var i=0;i<3;i++){
+        while(computerMap[secondShipStart-i] === 1 ||secondShipStart.toString().endsWith('1')||secondShipStart.toString().endsWith('2')){
+            secondShipStart = Math.floor(Math.random()*100)+1;
+        }
+    }
+    for(var i=0;i<3;i++){
+        computerMap[secondShipStart-i] = 1;
+    }
+    let thirdShipStart = Math.floor(Math.random()*100)+1;
+    for(var i=0;i<3;i++){
+        while(computerMap[thirdShipStart-i] === 1||thirdShipStart.toString().endsWith('1')||thirdShipStart.toString().endsWith('2')){
+            thirdShipStart = Math.floor(Math.random()*100)+1;
+        }
+    }
+    for(var i=0;i<3;i++){
+        computerMap[thirdShipStart-i] = 1;
+    }
+    let forthShipStart = Math.floor(Math.random()*100)+1;
+    for(var i=0;i<4;i++){
+        while(computerMap[forthShipStart-i] === 1||forthShipStart.toString().endsWith('1')||forthShipStart.toString().endsWith('2')||forthShipStart.toString().endsWith('3')){
+            forthShipStart = Math.floor(Math.random()*100)+1;
+        }
+    }
+    for(var i=0;i<4;i++){
+        computerMap[forthShipStart-i] = 1;
+    }
+    let fifthShipStart = Math.floor(Math.random()*100)+1;
+    for(var i=0;i<5;i++){
+        while(computerMap[fifthShipStart-i] === 1||fifthShipStart.toString().endsWith('1')||fifthShipStart.toString().endsWith('2')||fifthShipStart.toString().endsWith('3')||fifthShipStart.toString().endsWith('4')){
+            fifthShipStart = Math.floor(Math.random()*100)+1;
+        }
+    }
+    for(var i=0;i<5;i++){
+        computerMap[fifthShipStart-i] = 1;
+    }
+    console.log(computerMap)
+    for(const box in computerMap){
+        if(computerMap[box]===1){
+            $('#mainGrid #'+box).css('background-color','green')
         }
     }
 }
@@ -191,7 +244,6 @@ $('.square').click(function(evt){
         if(currentShipClicked==="ship2"){
         //checking if ship will be out of bounds. if box id is 1 dont run 
             if(!evt.target.id.endsWith('1')){
-                console.log(evt.target.id);
                 playerMap[evt.target.id]=1;
                 playerMap[evt.target.id-1] =1;
                 holdingShip=false;
@@ -201,8 +253,6 @@ $('.square').click(function(evt){
             }
         } else if(currentShipClicked==='ship3'){
             if(!evt.target.id.endsWith('1')&&!evt.target.id.endsWith('2')){
-                console.log(evt.target.id);
-                playerMap[evt.target.id]=1;
                 for(var i=0; i<3;i++){
                     playerMap[evt.target.id-i] =1;
                 }
@@ -213,8 +263,6 @@ $('.square').click(function(evt){
             }
         } else if(currentShipClicked==='secondShip3'){
             if(!evt.target.id.endsWith('1')&&!evt.target.id.endsWith('2')){
-                console.log(evt.target.id);
-                playerMap[evt.target.id]=1;
                 for(var i=0; i<3;i++){
                     playerMap[evt.target.id-i] =1;
                 }
@@ -225,8 +273,6 @@ $('.square').click(function(evt){
             }
         } else if(currentShipClicked==='ship4'){
             if(!evt.target.id.endsWith('1')&&!evt.target.id.endsWith('2')&&!evt.target.id.endsWith('3')){
-                console.log(evt.target.id);
-                playerMap[evt.target.id]=1;
                 for(var i=0; i<4;i++){
                     playerMap[evt.target.id-i] =1;
                 }
@@ -237,8 +283,6 @@ $('.square').click(function(evt){
             }
         } else if(currentShipClicked==='ship5'){
             if(!evt.target.id.endsWith('1')&&!evt.target.id.endsWith('2')&&!evt.target.id.endsWith('3')&&!evt.target.id.endsWith('4')){
-                console.log(evt.target.id);
-                playerMap[evt.target.id]=1;
                 for(var i=0; i<5;i++){
                     playerMap[evt.target.id-i] =1;
                 }
@@ -249,5 +293,7 @@ $('.square').click(function(evt){
                 allShipsPlaced();
             }
         }   
+    } else if(attackPhase===true){
+        console.log('here')
     }
 })
