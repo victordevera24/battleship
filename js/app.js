12df17bf7931a4ee32d3b3120ccd30eac5d05060
victordevera24 <victordevera24@gmail.com>
2022-04-11 10:56:13 -0700

    adding all ship place hover

--- js/app.js
+++ js/app.js
@@ -45,10 +45,10 @@ function createShip(num){
     })
 }
 function createSecond3LongShip(){
-    let newShip = `<div id="second3Ship" class="ship 3 ship3"></div>`;
+    let newShip = `<div id="secondShip3" class="ship 3 ship3"></div>`;
     $('#chooseShip').append(newShip);
     for (let i = 0; i < 3; i++) {
-        $('#second3Ship').append("<div id='pieceOfSecond3' class='shipSquare'></div>");
+        $('#secondShip3').append("<div id='pieceOfSecond3' class='shipSquare'></div>");
     }
 }
 // start game    -------------------------------------------------------
@@ -74,9 +74,87 @@ $('.square').hover(function(evt){
     let square = evt.target
     if(holdingShip===true){
         $(square).css('background-color','purple')
+        if(currentShipClicked==="ship2"){
+            if(square.id.endsWith('1')){
+                $(square).css('background-color','red')
+            } else {
+                let shipLength = square.id - 1;
+                console.log(shipLength)
+                $('#'+shipLength).css('background-color','purple')
+            }
+        } else if(currentShipClicked==="ship3"){
+            if(square.id.endsWith('2')||square.id.endsWith('1')){
+                $(square).css('background-color','red')
+            } else {
+                for (let i=0;i<3;i++) {
+                    let shipLength = square.id - i;
+                    console.log(shipLength)
+                    $('#'+shipLength).css('background-color','purple')
+                }
+            }
+        } else if(currentShipClicked==="secondShip3"){
+            if(square.id.endsWith('2')||square.id.endsWith('1')){
+                $(square).css('background-color','red')
+            } else {
+                for (let i=0;i<3;i++) {
+                    let shipLength = square.id - i;
+                    console.log(shipLength)
+                    $('#'+shipLength).css('background-color','purple')
+                }
+            }
+        } else if(currentShipClicked==="ship4"){
+            if(square.id.endsWith('2')||square.id.endsWith('1')||square.id.endsWith('3')){
+                $(square).css('background-color','red')
+            } else {
+                for (let i=0;i<4;i++) {
+                    let shipLength = square.id - i;
+                    console.log(shipLength)
+                    $('#'+shipLength).css('background-color','purple')
+                }
+            }
+        } else if(currentShipClicked==="ship5"){
+            if(square.id.endsWith('2')||square.id.endsWith('1')||square.id.endsWith('3')||square.id.endsWith('4')){
+                $(square).css('background-color','red')
+            } else {
+                for (let i=0;i<5;i++) {
+                    let shipLength = square.id - i;
+                    console.log(shipLength)
+                    $('#'+shipLength).css('background-color','purple')
+                }
+            }
+        }
     }
     },function(evt){
         let square = evt.target
         $(square).css('background-color','blue')
+        if(currentShipClicked==="ship2"){
+            let shipLength = square.id - 1;
+            console.log(shipLength)
+            $('#'+shipLength).css('background-color','blue')
+        } else if(currentShipClicked==="ship3"){
+            for(let i=0; i<3; i++){
+                let shipLength = square.id - i;
+                console.log(shipLength)
+                $('#'+shipLength).css('background-color','blue')
+            }
+        } else if(currentShipClicked==="secondShip3"){
+            for(let i=0; i<3; i++){
+                let shipLength = square.id - i;
+                console.log(shipLength)
+                $('#'+shipLength).css('background-color','blue')
+            }
+        } else if(currentShipClicked==="ship4"){
+            for(let i=0; i<4; i++){
+                let shipLength = square.id - i;
+                console.log(shipLength)
+                $('#'+shipLength).css('background-color','blue')
+            }
+        } else if(currentShipClicked==="ship5"){
+            for(let i=0; i<5; i++){
+                let shipLength = square.id - i;
+                console.log(shipLength)
+                $('#'+shipLength).css('background-color','blue')
+            }
+        }
     }
 )
